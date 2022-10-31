f0cb1e5eef269ec3f83011010ddcda0c3811f11b
delguanzon <yodelguanzon@gmail.com>
2022-10-31 11:12:55 -0700

    Add functionality to let user add tasks
    Co-authored-by: Matthew Belcher <Matthewjbelcher7@live.com>

--- README.md
+++ README.md
@@ -1,4 +1,17 @@
 Describe: toDoList()
 Test: "It should display a list of things to do"
 Code: toDoList();
-Expected Output: {0: "FirstThing to Do", 1:  "SecondThing to Do",  2:  "SecondThing to Do"}
+Expected Output:
+(2) [{...}, {...}, {...}]
+ {0: "FirstThing to Do", 
+ 1:  "SecondThing to Do",  
+ 2:  "SecondThing to Do"}
+
+ Describe: addTask()
+Test: "It should add a task in the list of things to do"
+Code: toDoList();
+Expected Output:
+(2) [{...}, {...}, {...}]
+ {0: "FirstThing to Do", 
+ 1:  "SecondThing to Do",  
+ 2:  "SecondThing to Do"}
--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,23 @@ let task2 = { name: "secondary thing to do"};
 let task3 = { name: "third thing to do"};
 let taskList = { name: "to do list", tasks: [task1, task2, task3] };
 
-function toDoList(task) {
-  const task = {};
+function ToDoList() {
+  this.taskList = {};
+  this.taskId = 0;
+}
+
+toDoList.prototype.addTask = function(taskObject) {
+  taskObject.taskId = this.assignId();
+  this.taskList[taskObject.taskId] = taskObject;
+}
+
+toDoList.prototype.assignId = function() {
+  this.taskId += 1;
+  return this.taskId;
+};
+
+function Task(taskName) {
+  this.taskName = taskName;
+  this.status = "ToDo";
 }
 
