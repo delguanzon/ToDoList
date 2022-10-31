2ac98e7e86ebcf535f74a3dc0ea96a2488d9f02b
delguanzon <yodelguanzon@gmail.com>
2022-10-31 10:27:32 -0700

    Display a list of things to do
    Co-authored-by: Matthew Belcher <Matthewjbelcher7@live.com>

---
+++ README.md
@@ -0,0 +1,4 @@
+Describe: toDoList()
+Test: "It should display a list of things to do"
+Code: toDoList();
+Expected Output: {0: "FirstThing to Do", 1:  "SecondThing to Do",  2:  "SecondThing to Do"}
---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+let task1 = { name: "first thing to do"};
+let task2 = { name: "secondary thing to do"};
+let task3 = { name: "third thing to do"};
+let taskList = { name: "to do list", tasks: [task1, task2, task3] };
+
+function toDoList(task) {
+  const task = {};
+}
+
